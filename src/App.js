2fb7022df6182502028100e74ae4346e52ee0631
quagh03 <03.quanghuy.le@gmail.com>
2023-11-30 13:50:08 +0700

    upload posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import axios from 'axios';
+import './MyEditor';
 
 const API_URL = "http://127.0.0.1:8080/api/blog/image/upload";
 
 export default function MyEditor({ ...props }) {
   const [editorData, setEditorData] = useState("");
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [metaTitle, setMetaTitle] = useState("");
+  const [slug, setSlug] = useState("");
+  const [published, setPublished] = useState(false);
+  const [thumbnail, setThumbnail] = useState("");
+  const [authorId] = useState(1);
+  const [parentId, setParentId] = useState(null);
+  const [categoryIds, setCategoryIds] = useState([]);
 
   const handleEditorChange = (data) => {
     const imgRegex = /<img.*?src="(.*?)"/g;
@@ -24,6 +35,31 @@ export default function MyEditor({ ...props }) {
     });
 
     setEditorData(data);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const blogPost = {
+      title,
+      summary,
+      metaTitle,
+      slug,
+      published,
+      content: editorData,
+      thumbnail,
+      authorId,
+      parentId,
+      categoryIds,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      publishedAt: published ? new Date().toISOString() : null,
+    };
+    try {
+      await axios.post('http://localhost:8080/api/blog/posts', blogPost);
+      alert('Blog post submitted successfully!');
+    } catch (error) {
+      console.error('Error submitting blog post:', error);
+    }
   };
 
   function uploadAdapter(loader) {
@@ -61,6 +97,39 @@ export default function MyEditor({ ...props }) {
 
   return (
     <div className="App">
+      <form onSubmit={handleSubmit} className="blog-form">
+        <label>
+          Title:
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required className="blog-input" />
+        </label>
+        <label>
+          Meta Title:
+          <input type="text" value={metaTitle} onChange={(e) => setMetaTitle(e.target.value)} required className="blog-input" />
+        </label>
+        <label>
+          Slug:
+          <input type="text" value={slug} onChange={(e) => setSlug(e.target.value)} required className="blog-input" />
+        </label>
+        <label>
+          Summary:
+          <textarea value={summary} onChange={(e) => setSummary(e.target.value)} required className="blog-input" />
+        </label>
+        <label>
+          Published:
+          <input type="checkbox" checked={published} onChange={(e) => setPublished(e.target.checked)} className="blog-input" />
+        </label>
+        <label>
+          Thumbnail URL:
+          <input type="text" value={thumbnail} onChange={(e) => setThumbnail(e.target.value)} className="blog-input" />
+        </label>
+        <label>
+          Parent Post ID:
+          <input type="text" value={parentId} onChange={(e) => setParentId(e.target.value)} className="blog-input" />
+        </label>
+        <label>
+          Category IDs (comma-separated):
+          <input type="text" value={categoryIds} onChange={(e) => setCategoryIds(e.target.value.split(','))} className="blog-input" />
+        </label>
         <CKEditor
             config={{
                 extraPlugins: [uploadPlugin],
@@ -74,6 +143,8 @@ export default function MyEditor({ ...props }) {
             }}
             {...props}
         />
+        <button type="submit" className="submit-button">Submit</button>
+      </form>
     </div>
   );
 }
